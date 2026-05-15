a1851f80d19dd37aae8b806b11a499f1c3500b19
Cristian <lcristianiim@gmail.com>
2026-05-15 21:44:58 +0300

    udpate changes for fedora

--- keyboard-setup.sh
+++ keyboard-setup.sh
@@ -1,4 +1,6 @@
-xset r rate 190 60
+#!/bin/bash
+# Keyboard repeat rate for GNOME/Wayland (Fedora)
+# Equivalent to old X11: xset r rate 190 60
 
-# gsettings set org.gnome.desktop.peripherals.keyboard delay 60
-# gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 190
+gsettings set org.gnome.desktop.peripherals.keyboard delay 190
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 17
